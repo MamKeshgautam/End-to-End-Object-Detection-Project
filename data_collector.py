cfb58f37b51ae73d1d91690846fc4b3e4437efe9
MamKeshgautam <mamkeshgautam@gmail.com>
2024-04-16 23:36:13 +0530

    datacollection added

---
+++ data_collector.py
@@ -0,0 +1,39 @@
+import cv2
+import os
+import time 
+import uuid
+
+
+IMAGE_PATH="collected_image"
+
+labels=["Hello","Yes","No",'Thanks',"ILoveYou","Please"]
+
+number_of_images=5
+
+for label in labels:
+    img_path=os.path.join(IMAGE_PATH,label)
+
+    os.makedirs(img_path)
+
+
+    #open_camera
+    cap=cv2.VideoCapture(0)
+
+    print(f'collecting image for {label}')
+
+    time.sleep(4)
+
+    for imgnum in range(number_of_images):
+        ref, frame =cap.read()
+
+        imagename=os.path.join(IMAGE_PATH,label,label+'.'+'{}.jpg'.format(str(uuid.uuid1())))
+        cv2.imwrite(imagename,frame)
+        cv2.imshow('frame',frame)
+        time.sleep(2)
+
+
+        if cv2.waitKey(1) & 0xFF==ord('q'):
+            break
+
+    
+    cap.release()
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools  import find_packages,setup
+
+setup(
+    name ='SignLanguages',
+    version='0.0.0',
+    author="Mamkesh Kumar Gautam",
+    packages=find_packages(),
+    install_requires=[]
+)
